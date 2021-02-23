b11276e109f0050c1da5fa533734d0f37c62d3d5
NozomuTsuruta <tuno.0806@icloud.com>
2021-02-23 17:05:04 +0900

    add: 簡単な音声認識追加

--- src/hooks/useSpeech.ts
+++ src/hooks/useSpeech.ts
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { IQuestion } from "../redux/questions/types";
 import Router from "next/router";
+import { IAnswer } from "../redux/answers/type";
 
 const useSpeech = (
   question: IQuestion,
@@ -10,24 +11,29 @@ const useSpeech = (
   const [paused, setPaused] = useState(false);
   const [synth, setSynth] = useState<SpeechSynthesis>();
   const [count, setCount] = useState<number>(60);
+  const [uttr, setUttr] = useState<SpeechSynthesisUtterance>();
+  const [answer, setAnswer] = useState<IAnswer[]>([]);
 
   useEffect(() => {
     const synth = window.speechSynthesis;
-    setSynth(synth);
-  }, []);
-
-  useEffect(() => {
-    if (!process.browser || !question || loading || paused || !synth) {
-      return;
-    }
     const uttr = new SpeechSynthesisUtterance();
     const voices = synth.getVoices();
     uttr.voice = voices[0]; // 0, 58
     uttr.lang = "ja-JP";
     uttr.pitch = 0;
     uttr.rate = 0.8;
+    setSynth(synth);
+    setUttr(uttr);
+  }, []);
+
+  console.log(answer);
+
+  useEffect(() => {
+    if (!question || loading || paused || !synth || !uttr) {
+      return;
+    }
     uttr.text = question.text;
-    synth?.speak(uttr);
+    synth.speak(uttr);
     const timer = setTimeout(() => {
       setQuestionNum((prev) => prev + 1);
     }, question.second * 1000);
@@ -39,8 +45,39 @@ const useSpeech = (
   }, [question, loading, paused]);
 
   useEffect(() => {
-    setCount(60);
+    setCount(question.second);
   }, [question]);
+
+  useEffect(() => {
+    if (synth?.speaking) {
+      return;
+    }
+    const rec = new webkitSpeechRecognition();
+    rec.start();
+    rec.onresult = (e) => {
+      rec.stop();
+      console.log(e.results[0][0].transcript);
+      const answer = e.results[0][0].transcript;
+      setAnswer((prev) => {
+        if (prev.some(({ id }) => id === question.id)) {
+          return prev.map((el) => {
+            if (el.id === question.id) {
+              return {
+                id: el.id,
+                question: el.question,
+                answer: el.answer + answer,
+              };
+            }
+            return el;
+          });
+        }
+        return [
+          ...prev,
+          { id: question.id, question: question.text, answer: answer },
+        ];
+      });
+    };
+  }, [synth?.speaking]);
 
   const pause = () => {
     setPaused(true);
---
+++ src/redux/answers/type.ts
@@ -0,0 +1,5 @@
+export type IAnswer = {
+  id: string;
+  question: string;
+  answer: string;
+};
